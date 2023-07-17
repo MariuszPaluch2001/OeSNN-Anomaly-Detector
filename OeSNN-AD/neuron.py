9da2ce3c1d7f7bc998f036265ac7eb007bf823c8
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-07-17 20:54:56 +0200

    Format OeSNN-AD with autopep8.

--- OeSNN-AD/neuron.py
+++ OeSNN-AD/neuron.py
@@ -1,18 +1,21 @@
 import numpy as np
 
+
 class Neuron:
     def __init__(self) -> None:
         pass
 
+
 class Input_Neuron(Neuron):
-    def __init__(self, firing_time : float) -> None:
+    def __init__(self, firing_time: float) -> None:
         super().__init__()
         self.firing_time = firing_time
 
+
 class Output_Neuron(Neuron):
-    def __init__(self, weights : np.ndarray, gamma : float, 
-                 output_value : float, M: float, addition_time : float, 
-                 PSP : float, max_PSP : float) -> None:
+    def __init__(self, weights: np.ndarray, gamma: float,
+                 output_value: float, M: float, addition_time: float,
+                 PSP: float, max_PSP: float) -> None:
         super().__init__()
         self.weights = weights
         self.gamma = gamma
@@ -21,9 +24,12 @@ class Output_Neuron(Neuron):
         self.addition_time = addition_time
         self.PSP = PSP
         self.max_PSP = max_PSP
-    
-    def update_neuron(self, candidate_neuron : 'Output_Neuron') -> None:
-        self.weights = (candidate_neuron.weights + self.M * self.weights) / (self.M + 1)
-        self.output_value = (candidate_neuron.output_value + self.M * self.output_value) / (self.M + 1)
-        self.addition_time = (candidate_neuron.addition_time + self.M * self.addition_time) / (self.M + 1)
+
+    def update_neuron(self, candidate_neuron: 'Output_Neuron') -> None:
+        self.weights = (candidate_neuron.weights +
+                        self.M * self.weights) / (self.M + 1)
+        self.output_value = (candidate_neuron.output_value +
+                             self.M * self.output_value) / (self.M + 1)
+        self.addition_time = (
+            candidate_neuron.addition_time + self.M * self.addition_time) / (self.M + 1)
         self.M += 1
--- OeSNN-AD/test_GRF.py
+++ OeSNN-AD/test_GRF.py
@@ -1,25 +1,28 @@
 from grf_init import GRF_Init
 import numpy as np
 
-WINDOW = np.array([0.5, 0.3, 0.4, 
-                   0.3, 0.6, 0.2, 
-                   1.0, 0.4, 0.3, 
-                   0.4, 0.2, 0.4, 
+WINDOW = np.array([0.5, 0.3, 0.4,
+                   0.3, 0.6, 0.2,
+                   1.0, 0.4, 0.3,
+                   0.4, 0.2, 0.4,
                    0.1, 0.5])
+
 
 def test__get_width_vec():
     grf = GRF_Init(WINDOW, 7, 1, 0.5)
-    
+
     result = grf._get_width_vec()
     correct = np.repeat(0.18, 7)
-    np.testing.assert_array_almost_equal(result, correct, decimal = 2)
+    np.testing.assert_array_almost_equal(result, correct, decimal=2)
+
 
 def test__get_center_vec():
     grf = GRF_Init(WINDOW, 7, 1, 0.5)
-    
+
     result = grf._get_center_vec()
     correct = np.array([-0.17,  0.01,  0.19,  0.37,  0.55,  0.73,  0.91])
-    np.testing.assert_array_almost_equal(result, correct, decimal = 2)
+    np.testing.assert_array_almost_equal(result, correct, decimal=2)
+
 
 def test__get_excitation():
     grf = GRF_Init(WINDOW, 7, 1, 0.5)
@@ -28,7 +31,8 @@ def test__get_excitation():
 
     result = grf._get_excitation(width_v, center_v)
     correct = np.array([0.001, 0.024, 0.227, 0.770, 0.962, 0.442, 0.074])
-    np.testing.assert_array_almost_equal(result, correct, decimal = 3)
+    np.testing.assert_array_almost_equal(result, correct, decimal=3)
+
 
 def test__get_firing_time():
     grf = GRF_Init(WINDOW, 7, 1, 0.5)
@@ -36,21 +40,23 @@ def test__get_firing_time():
 
     result = grf._get_firing_time(excitation)
     correct = np.array([0.999, 0.976, 0.773, 0.230, 0.038, 0.558, 0.926])
-    np.testing.assert_array_almost_equal(result, correct, decimal = 3)
+    np.testing.assert_array_almost_equal(result, correct, decimal=3)
+
 
 def test__get_order():
     grf = GRF_Init(WINDOW, 7, 1, 0.5)
     firing_time = np.array([0.999, 0.976, 0.773, 0.230, 0.038, 0.558, 0.926])
 
     result = grf._get_order(firing_time)
-    correct = np.array([6,5,3,1,0,2,4])
+    correct = np.array([6, 5, 3, 1, 0, 2, 4])
     np.testing.assert_array_equal(result, correct)
+
 
 def test_get_order():
     grf = GRF_Init(WINDOW, 7, 1, 0.5)
 
     result = grf.get_order()
-    correct = np.array([6,5,3,1,0,2,4])
+    correct = np.array([6, 5, 3, 1, 0, 2, 4])
     np.testing.assert_array_equal(result, correct)
 
 # def test__get_weights():
