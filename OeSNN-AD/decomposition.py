89f3cd7177c293a529e72eb51ba4e722f581d462
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-10-11 20:31:38 +0200

    Add simple OeSNNAD to decomposition

--- OeSNN-AD/decomposition.py
+++ OeSNN-AD/decomposition.py
@@ -1,27 +1,29 @@
-from oesnn_ad import OeSNNAD
-from PyEMD import CEEMDAN
-import numpy.typing as npt
+import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
+from PyEMD import CEEMDAN
+
+from oesnn_ad import OeSNNAD
+
 
 class DecomposeOeSNNAD:
     
-    def __init__(self, stream: npt.NDArray[np.float64]) -> None:
-        self.ceemdan = CEEMDAN(range_thr=0.001, total_power_thr=0.01)
-        self.c_imfs = self.ceemdan(stream, max_imf=10)
-        print(self.c_imfs)
+    def __init__(self, stream: npt.NDArray[np.float64],
+                 range_threshold= 1e-3,
+                 total_power_threshold= 1e-2) -> None:
+        self.stream = stream
+        self.ceemdan = CEEMDAN(range_thr=range_threshold, total_power_thr=total_power_threshold)
     
-    def _decompose(self):
-        ...
+    def _decompose(self, channels: int):
+        return self.ceemdan.ceemdan(self.stream, max_imf=channels)
         
-    def predict(self):
-        ...
+    def predict(self, tuning_type: str, cemmndan_channels: int=-1):
+        c_imfs = self._decompose(cemmndan_channels)
+        results = [OeSNNAD(component, window_size=4).predict() for component in c_imfs]
+        print(results)
+        return results
+            
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt        
     x = np.array([1,2,4,6,7,63,23,12,342,5,63,21])
-    res = DecomposeOeSNNAD(np.array([1,2,4,6,7,63,23,12,342,5,63,21])).c_imfs
-    print(res.transpose())
-    plt.plot(x)
-    plt.plot(res.transpose())
-    plt.savefig(f"output.png")
-    plt.close()
+    res = DecomposeOeSNNAD(x).predict('whatever')
