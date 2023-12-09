a91b878254df3ee9a044f2be7a4f8abffc69ce7c
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-12-09 20:02:54 +0100

    Push CEEMDAN decomposition

--- OeSNN-AD/decomposition.py
+++ OeSNN-AD/decomposition.py
@@ -1,3 +1,4 @@
+from utils import get_data_from_path, perf_measure
 import numpy as np
 import numpy.typing as npt
 from PyEMD import CEEMDAN
@@ -9,33 +10,28 @@ from threading import Thread
 class DecomposeOeSNNAD:
 
     def __init__(self, stream: npt.NDArray[np.float64],
+                 trials=100,
                  range_threshold=1e-3,
                  total_power_threshold=1e-2) -> None:
         self.stream = stream
-        self.ceemdan = CEEMDAN(range_thr=range_threshold,
-                               total_power_thr=total_power_threshold)
+        self.ceemdan = CEEMDAN(trials=trials, range_threshold=range_threshold,
+                               total_power_threshold=total_power_threshold)
 
     def _decompose(self, channels: int):
         return self.ceemdan.ceemdan(self.stream, max_imf=channels)
 
-    def _merge(self, streams) -> npt.NDArray:
-        # TODO: merge streams after decomposition
-        return streams
+    def _merge(self, streams: npt.NDArray) -> npt.NDArray:
+        summed_columns = streams.sum(axis=0)
+        return (summed_columns / streams.shape[0]) > 0.5
 
-    def predict(self, tuning_type: str, cemmndan_channels: int = -1):
+    def predict(self, cemmndan_channels: int = -1, **parameters):
         c_imfs = self._decompose(cemmndan_channels)
         streams = []
         threads = [Thread(
-            target=streams.append(OeSNNAD(component, window_size=4).predict())
+            target=streams.append(OeSNNAD(component, **parameters).predict())
         ) for component in c_imfs]
         for t in threads:
             t.start()
         for t in threads:
             t.join()
-        return self._merge(streams)
-
-
-if __name__ == "__main__":
-    x = np.array([1, 2, 4, 6, 7, 63, 23, 12, 342, 5, 63, 21])
-    res = DecomposeOeSNNAD(x).predict('whatever')
-    print(res)
+        return self._merge(np.array(streams))
---
+++ OeSNN-AD/main_decomposition.py
@@ -0,0 +1,194 @@
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import time
+
+from decomposition import DecomposeOeSNNAD
+from utils import get_data_from_path, perf_measure, get_all_files_paths
+import pandas as pd
+
+if __name__ == "__main__":
+
+    df_yahoo = pd.read_csv("results/yahoo_result.csv")
+    df_nab = pd.read_csv("results/nab_result.csv")
+
+    path = "/mnt/c/workspace/oesnn/OeSNN-Anomaly-Detector/data/NAB/realTweets/realTweets_Twitter_volume_UPS.csv"
+    data, labels = get_data_from_path(path, True)
+
+    with open("results/decomp_nab_result.csv", "w", encoding='UTF8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(("dataset", "filename", "f1", "recall",
+                        "precission", "time [s]"))
+        for path in get_all_files_paths("../data/NAB"):
+            dataset = path.split("/")[-2]
+            filename = path.split("/")[-1][:-4]
+
+            print(f"Stream: {dataset}/{filename}")
+
+            stream, labels = get_data_from_path(path, True)
+
+            df_stream = df_nab[(df_nab["dataset"] == dataset) & (
+                df_nab["filename"] == filename)]
+            df_stream.reset_index(drop=True, inplace=True)
+            df_stream = df_stream.loc[0]
+            if df_stream['f1'] == 0.0:
+                continue
+            t = time.time()
+            res = DecomposeOeSNNAD(stream, trials=100).predict(cemmndan_channels=-1,
+                                                               window_size=df_stream["Wsize"].astype(
+                                                                   int),
+                                                               num_out_neurons=df_stream["NOsize"].astype(
+                                                                   int),
+                                                               num_in_neurons=df_stream["NIsize"].astype(
+                                                                   int),
+                                                               beta=df_stream["Beta"].astype(
+                                                                   float),
+                                                               ts_factor=df_stream["TS"].astype(
+                                                                   float),
+                                                               sim=df_stream["sim"].astype(
+                                                                   float),
+                                                               mod=df_stream["mod"].astype(
+                                                                   float),
+                                                               c_factor=df_stream["C"].astype(
+                                                                   float),
+                                                               ksi=df_stream["ksi"].astype(
+                                                                   float),
+                                                               epsilon=df_stream["epsilon"].astype(int))
+
+            time_res = time.time() - t
+            print(f"Time: {time_res} sec")
+            recall, precission, f1 = perf_measure(res, labels)
+            print(f"F1 {f1}, Recall {recall}, Precission {precission}")
+
+            writer.writerow((dataset, filename, f1, recall,
+                             precission, time_res))
+            fn_list = []
+            fp_list = []
+            tn_list = []
+            tp_list = []
+            for index, (value, predict, label) in enumerate(
+                    zip(stream.tolist(), res.tolist(), labels.tolist())):
+                if predict and label:
+                    tp_list.append((index, value))
+                if predict and not label:
+                    fp_list.append((index, value))
+                if not predict and not label:
+                    tn_list.append((index, value))
+                if not predict and label:
+                    fn_list.append((index, value))
+            plt.figure(figsize=(15, 5))
+            plt.title(filename)
+            plt.plot([x[0] for x in fn_list], [x[1]
+                                               for x in fn_list], '+g', linestyle='None')
+            plt.plot([x[0] for x in fp_list], [x[1]
+                                               for x in fp_list], 'sb', linestyle='None')
+            plt.plot([x[0] for x in tn_list], [x[1]
+                                               for x in tn_list], '.k', linestyle='None')
+            plt.plot([x[0] for x in tp_list], [x[1]
+                                               for x in tp_list], '2r', linestyle='None')
+            plt.grid()
+            green_plus = mlines.Line2D([], [], color='green', marker='+', linestyle='None',
+                                       markersize=10, label='FN')
+            blue_square = mlines.Line2D([], [], color='blue', marker='s', linestyle='None',
+                                        markersize=10, label='FP')
+            black_dot = mlines.Line2D([], [], color='black', marker='.', linestyle='None',
+                                      markersize=10, label='TN')
+
+            red_triangle = mlines.Line2D([], [], color='red', marker='2', linestyle='None',
+                                         markersize=10, label='TP')
+            plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0, handles=[
+                green_plus, blue_square, black_dot, red_triangle])
+            plt.xlabel("Timestamp")
+            plt.ylabel("Input value (X)")
+            plt.savefig(f"plots-decomposition/NAB-{filename}.png")
+            plt.close()
+
+    with open("results/decomp_yahoo_result.csv", "w", encoding='UTF8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(("dataset", "filename", "f1", "recall",
+                        "precission", "time [s]"))
+        for path in get_all_files_paths("../data/Yahoo"):
+            dataset = path.split("/")[-2]
+            filename = path.split("/")[-1][:-4]
+
+            print(f"Stream: {dataset}/{filename}")
+
+            stream, labels = get_data_from_path(path, False)
+
+            df_stream = df_yahoo[(df_yahoo["dataset"] == dataset) & (
+                df_yahoo["filename"] == filename)]
+            df_stream.reset_index(drop=True, inplace=True)
+            df_stream = df_stream.loc[0]
+            if df_stream['f1'] == 0.0:
+                continue
+            t = time.time()
+            res = DecomposeOeSNNAD(stream, trials=100).predict(cemmndan_channels=-1,
+                                                               window_size=df_stream["Wsize"].astype(
+                                                                   int),
+                                                               num_out_neurons=df_stream["NOsize"].astype(
+                                                                   int),
+                                                               num_in_neurons=df_stream["NIsize"].astype(
+                                                                   int),
+                                                               beta=df_stream["Beta"].astype(
+                                                                   float),
+                                                               ts_factor=df_stream["TS"].astype(
+                                                                   float),
+                                                               sim=df_stream["sim"].astype(
+                                                                   float),
+                                                               mod=df_stream["mod"].astype(
+                                                                   float),
+                                                               c_factor=df_stream["C"].astype(
+                                                                   float),
+                                                               ksi=df_stream["ksi"].astype(
+                                                                   float),
+                                                               epsilon=df_stream["epsilon"].astype(int))
+
+            time_res = time.time() - t
+            print(f"Time: {time_res} sec")
+            recall, precission, f1 = perf_measure(res, labels)
+            print(f"F1 {f1}, Recall {recall}, Precission {precission}")
+
+            writer.writerow((dataset, filename, f1, recall,
+                             precission, time_res))
+            fn_list = []
+            fp_list = []
+            tn_list = []
+            tp_list = []
+            for index, (value, predict, label) in enumerate(
+                    zip(stream.tolist(), res.tolist(), labels.tolist())):
+                if predict and label:
+                    tp_list.append((index, value))
+                if predict and not label:
+                    fp_list.append((index, value))
+                if not predict and not label:
+                    tn_list.append((index, value))
+                if not predict and label:
+                    fn_list.append((index, value))
+            plt.figure(figsize=(15, 5))
+            plt.title(filename)
+            plt.plot([x[0] for x in fn_list], [x[1]
+                                               for x in fn_list], '+g', linestyle='None')
+            plt.plot([x[0] for x in fp_list], [x[1]
+                                               for x in fp_list], 'sb', linestyle='None')
+            plt.plot([x[0] for x in tn_list], [x[1]
+                                               for x in tn_list], '.k', linestyle='None')
+            plt.plot([x[0] for x in tp_list], [x[1]
+                                               for x in tp_list], '2r', linestyle='None')
+            plt.grid()
+            green_plus = mlines.Line2D([], [], color='green', marker='+', linestyle='None',
+                                       markersize=10, label='FN')
+            blue_square = mlines.Line2D([], [], color='blue', marker='s', linestyle='None',
+                                        markersize=10, label='FP')
+            black_dot = mlines.Line2D([], [], color='black', marker='.', linestyle='None',
+                                      markersize=10, label='TN')
+
+            red_triangle = mlines.Line2D([], [], color='red', marker='2', linestyle='None',
+                                         markersize=10, label='TP')
+            plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0, handles=[
+                green_plus, blue_square, black_dot, red_triangle])
+            plt.xlabel("Timestamp")
+            plt.ylabel("Input value (X)")
+            plt.savefig(f"plots-decomposition/Yahoo-{filename}.png")
+            plt.close()
